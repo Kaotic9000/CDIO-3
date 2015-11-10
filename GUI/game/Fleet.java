46698dc0540531ae0ba4bd24cf44fa4aab984fc5
Nicki N <nickinylin@hotmail.com>
2015-11-10 16:00:46 +0100

    Har opdateret disse klasser med arv fra Ownable

--- GUI/game/Fleet.java
+++ GUI/game/Fleet.java
@@ -1,6 +1,7 @@
 package game;
 
-public class Fleet {
+public class Fleet extends Ownable {
+	
 private int RENT_1 = 500;
 private int RENT_2 = 1000;
 private int RENT_3 = 2000;
--- GUI/game/Labor.java
+++ GUI/game/Labor.java
@@ -1,5 +1,5 @@
 package game;
 
-public class Labor {
+public class Labor extends Ownable {
 private int baserent;
 }
