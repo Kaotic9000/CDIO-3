dc6e873a3cbcd5d7520fc0539a5698156ea45bd3
Anthony <Anthony@10.16.106.152>
2015-11-10 15:30:08 +0100

    Game har fået lavet lavet forbindelse til Field og Dice klasserne, samt der bliver oprettet spillere og sat penge i banken

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,10 +1,38 @@
 package game;
 
+import desktop_resources.GUI;
+
 public class Game {
 
+	private static Fields[] list;
+	private static Dice[] dice = { new Dice(), new Dice() };
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Game();
+	}
+	
+	public Game() {
+		// Create Fields
+        Fields Field = new Fields();
+		
+        // How many Players?
+        
+        
+        // Create Player 1,2,3,4,5,6
+        Player Player1 = new Player();
+        Player Player2 = new Player();
+        Player Player3 = new Player();
+        Player Player4 = new Player();
+        Player Player5 = new Player();
+        Player Player6 = new Player();
+        
+        // Create Players Bank with start Money
+        Bank Player1Bank = new Bank(Player1.PlayerName);
+        Bank Player2Bank = new Bank(Player2.PlayerName);
+        Bank Player3Bank = new Bank(Player3.PlayerName);
+        Bank Player4Bank = new Bank(Player4.PlayerName);
+        Bank Player5Bank = new Bank(Player5.PlayerName);
+        Bank Player6Bank = new Bank(Player6.PlayerName);
 	}
 
 }
